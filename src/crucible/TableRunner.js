983d5cb36ee27fc6206813fa8233f85b8b4725a8
Eric Naeseth <enaeseth@gmail.com>
2008-12-29 14:37:10 -0600

    Finishing up the table runner.

--- src/crucible/TableRunner.js
+++ src/crucible/TableRunner.js
@@ -24,7 +24,11 @@ Crucible.TableRunner = Crucible.Class.create(Crucible.Runner, {
 	// Creates a new table runner.
 	initialize: function TableRunner(product, tests) {
 		var build = Crucible.Tools.element;
+		var runner = this;
 		TableRunner.superclass.initialize.call(this, product, tests);
+		
+		Crucible.Tools.addStyleSheet(Crucible.base +
+			'/assets/table_runner.css');
 		
 		this.root = build('div', {id: 'crucible_results'});
 		this.root.appendChild('h1', {}, this.product);
@@ -35,9 +39,65 @@ Crucible.TableRunner = Crucible.Class.create(Crucible.Runner, {
 		
 		this.startButton = build('div', {id: 'crucible_start'},
 			'Start Testing');
+		Crucible.observe(this.startButton, 'click', function() {
+			runner.run();
+		});
 		this.root.appendChild(this.startButton);
 		
+		this._listenForEvents();
 		document.body.insertBefore(this.root, document.body.firstChild);
+	},
+	
+	_listenForEvents: function _table_runner_listen_for_events() {
+		var events = {
+			run: 'Started',
+			pass: 'Passed',
+			fail: 'Failed',
+			exception: 'ThrewException'
+		};
+		var name;
+		for (name in events) {
+			this.events[name].add(this, '_test' + events[name]);
+		}
+		this.events.started.add(this, '_startedTesting');
+		this.events.finished.add(this, '_finishedTesting');
+	},
+	
+	_startedTesting: function _started_testing() {
+		var build = Crucible.Tools.element;
+		this.table = build('table', {'class': 'tests'});
+		this.startButton.parentNode.replaceChild(this.table, this.startButton);
+	},
+	
+	_finishedTesting: function _finished_testing() {
+		// TODO: build results table
+	},
+	
+	_testStarted: function _test_started(test) {
+		var build = Crucible.Tools.element;
+		var row = build('tr', {'class': 'busy'});
+		var cell = build('td', {'class': 'message'});
+		cell.innerHTML = 'Running &ldquo;' + test.name + '&rdquo;&hellip;';
+		row.appendChild(cell);
+		this.currentRow = row;
+	},
+	
+	_updateStatus: function _update_test_status(status, message) {
+		this.currentRow.className = status;
+		this.currentRow.firstChild.innerHTML = message;
+	},
+	
+	_testPassed: function _test_passed(test) {
+		this._updateStatus('pass', test.name);
+	},
+	
+	_testFailed: function _test_failed(test, info) {
+		this.updateStatus('fail', test.name + ': ' + info.description);
+	},
+	
+	_testThrewException: function _test_threw_exception(test, ex) {
+		this.updateStatus('exception', ex.name + ' in test &ldquo;' +
+			test.name + '&rdquo;: ' + ex.message);
 	},
 	
 	_statusIcon: function _make_status_icon(which) {
