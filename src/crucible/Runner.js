1eba1ac82fbf622fbaa9a480fd8d31eebf403cfb
Eric Naeseth <enaeseth@gmail.com>
2008-12-29 13:50:36 -0600

    Implement Runner.

--- src/crucible/Runner.js
+++ src/crucible/Runner.js
@@ -1,6 +1,7 @@
-// Class: Loki.Runner
+// Class: Crucible.Runner
 // Runs Crucible tests.
-Loki.Runner = Loki.Class.create({
+Crucible.Runner = Crucible.Class.create({
+	product: null,
 	tests: null,
 	
 	events: null,
@@ -10,10 +11,15 @@ Loki.Runner = Loki.Class.create({
 	//
 	// Parameters:
 	//     (Crucible.Test[]) tests - the tests to be run
-	initialize: function Runner(tests) {
-		this.tests = tests;
+	initialize: function Runner(product, tests) {
+		this.product = product || '(unknown product)';
+		this.tests = [];
+		if (tests) {
+			Crucible.forEach(tests, this.add, this);
+		}
 		
 		this.events = {
+			start: new Crucible.Delegator("started testing"),
 			run: new Crucible.Delegator("test started"),
 			pass: new Crucible.Delegator("test passed"),
 			fail: new Crucible.Delegator("test failed"),
@@ -21,5 +27,53 @@ Loki.Runner = Loki.Class.create({
 			result: new Crucible.Delegator("test finished"),
 			finish: new Crucible.Delegator("finished testing")
 		};
+	},
+	
+	// Method: add
+	// Adds a test to the runner.
+	//
+	// Parameters:
+	//     (Crucible.Test) test - the test to add
+	add: function add_to_runner(test) {
+		test.events.result.add(this, '_processResult');
+		this.tests.push(test);
+	},
+	
+	// Method: run
+	// Runs the tests.
+	run: function run_tests() {
+		if (this.running) {
+			throw new Error('Already running!');
+		}
+		
+		this.queue = this.tests.slice(0); // clone
+		this.queue.reverse();
+		this.running = true;
+		
+		this.events.start.call();
+		this._runTest();
+	},
+	
+	_runTest: function _runner_run_test() {
+		if (this.queue.length == 0) {
+			this.events.finish.call();
+			this.running = false;
+			delete this.queue;
+			return;
+		}
+		
+		var test = this.queue.pop();
+		this.events.run.call(test);
+		Crucible.defer(function() {
+			test.run();
+		});
+	},
+	
+	_processResult: function _process_test_result(test, status, result) {
+		this.events[status].call(test, result || null);
+		this.events.result.call(test, status, result || null);
+		Crucible.defer(function() {
+			this._runTest();
+		});
 	}
 });
