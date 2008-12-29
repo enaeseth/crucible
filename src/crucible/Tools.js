34ff1f771f0709caa1fb25572769e7143f3bc5bb
Eric Naeseth <enaeseth@gmail.com>
2008-12-29 11:43:10 -0600

    Adding a Crucible.Tools.trim function.

--- src/crucible/Tools.js
+++ src/crucible/Tools.js
@@ -44,6 +44,17 @@ Crucible.Tools = {
 		return result;
 	},
 	
+	trim: function trim_string(str) {
+		str = str.replace(/^\s+/, '');
+		for (var i = str.length - 1; i >= 0; i--) {
+			if (/\S/.test(str.charAt(i))) {
+				str = str.substring(0, i + 1);
+				break;
+			}
+		}
+		return str;
+	},
+	
 	/**
 	 * Returns the attributes of an element.
 	 * @param {Element}	elem
